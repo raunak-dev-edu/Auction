cec63705f7ac4c4dabe8f76f15d101dce22a5841
Raunak Kumar Gupta <95216822+raunak-dev-edu@users.noreply.github.com>
2023-03-18 19:45:27 +0530

    add vercel config

--- auction/settings.py
+++ auction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cmj=5c(jxxse((#c#y=%8_)+-7&xv05v$hxrzkv8*oi_%-$ykj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [ BASE_DIR / "main" ]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
